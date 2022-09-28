1891790c9e8fc7886e3c50d4cff42a7dbb6d1781
joagaloppo <joaquingaloppo@gmail.com>
2022-09-28 18:55:23 -0300

    Change in package.json

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize( `${process.env.DATABASE_URL}?sslmode=no-verify` || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
